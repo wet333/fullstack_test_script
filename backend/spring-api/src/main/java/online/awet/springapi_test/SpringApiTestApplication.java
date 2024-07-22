e01920108202af96ff2de5500df0ace1b044c535
wet333 <wet.4gustin@gmail.com>
2024-07-22 20:37:58 -0300

    Initial project setup

---
+++ backend/spring-api/src/main/java/online/awet/springapi_test/SpringApiTestApplication.java
@@ -0,0 +1,13 @@
+package online.awet.springapi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTestApplication.class, args);
+	}
+
+}
---
+++ backend/spring-api/src/test/java/online/awet/springapi_test/SpringApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package online.awet.springapi_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
